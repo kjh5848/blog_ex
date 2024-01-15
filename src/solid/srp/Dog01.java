473e73f242413220351e91aea03180d3f1555fea
kjh5848~ <kjh5848@gmail.com>
2024-01-15 11:52:17 +0900

    ãSRP ãex01 dog

---
+++ src/solid/srp/Dog01.java
@@ -0,0 +1,20 @@
+package solid.srp;
+
+// 옳지 않은 예제
+public class Dog01 {
+    private String name;
+    private int age;
+
+    // 짖기
+    public void bark() {
+
+    }
+    // 먹이먹기
+    public void feed() {
+
+    }
+
+}
+
+
+
---
+++ src/solid/srp/Dog02.java
@@ -0,0 +1,24 @@
+package solid.srp;
+
+
+// SRP 적용 예제
+public class Dog02 {
+    private String name;
+    private int age;
+
+}
+
+// 짖기
+ class Dog02Voice {
+    public void bark(Dog02 dog02) {
+
+    }
+}
+
+// 먹이먹기
+class Dog02Feeding {
+    public void feed(Dog02 dog02) {
+
+    }
+}
+
